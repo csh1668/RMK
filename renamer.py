9c3390aee1eb4035b8939a58906140f66c75ac2c
SeoHyeon <tjgus1668@naver.com>
2023-11-14 13:42:14 +0900

    test

--- renamer.py
+++ renamer.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 MAX_INDEX = 100001
 RKM_DATA_PATH = os.path.join(os.getcwd(), 'Data')
@@ -30,19 +31,24 @@ def get_empty_idx() -> int:
     return -1
 
 
-for (root, dirs, files) in os.walk(RKM_DATA_PATH):
-    for file_name in files:
-        if '.xml' in file_name:
-            if file_name[:-4].isdecimal():
-                set(int(file_name[:-4]), True)
+if __name__ == '__main__':
+    for (root, dirs, files) in os.walk(RKM_DATA_PATH):
+        for file_name in files:
+            if '.xml' in file_name:
+                if file_name[:-4].isdecimal():
+                    set(int(file_name[:-4]), True)
 
-for (root, dirs, files) in os.walk(RKM_DATA_PATH):
-    for file_name in files:
-        if '.xml' in file_name and not file_name[:-4].isdecimal():
-            src = os.path.join(root, file_name)
-            dest = os.path.join(root, '{0:05d}.xml'.format(int(get_empty_idx())))
-            print(f'{src} -> {os.path.basename(dest)}')
-            os.rename(src, dest)
-            set(get_empty_idx())
+    cnt_changed: int = 0
+    for (root, dirs, files) in os.walk(RKM_DATA_PATH):
+        for file_name in files:
+            if '.xml' in file_name and not file_name[:-4].isdecimal():
+                src = os.path.join(root, file_name)
+                dest = os.path.join(root, '{0:05d}.xml'.format(int(get_empty_idx())))
+                print(f'{src} -> {os.path.basename(dest)}')
+                os.rename(src, dest)
+                set(get_empty_idx())
+                cnt_changed += 1
 
-print('Done!')
+    print('Done!')
+    if cnt_changed == 0:
+        exit(1)
